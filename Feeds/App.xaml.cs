3955998c618296ed4d15b063c14631b5e65ae10b
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2025-01-16 21:07:26 +0100

    Initial commit

---
+++ Feeds/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Feeds
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            Environment.SetEnvironmentVariable("WEBVIEW2_DEFAULT_BACKGROUND_COLOR", "0");
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    rootFrame.Navigate(typeof(MainPage));
+                }
+                Window.Current.Activate();
+            }
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            deferral.Complete();
+        }
+    }
+}
---
+++ Feeds/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using muxc = Microsoft.UI.Xaml.Controls;
+
+namespace Feeds
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage() => InitializeComponent();
+
+        private void WebView2Control_CoreWebView2Initialized(muxc.WebView2 sender, muxc.CoreWebView2InitializedEventArgs args)
+        {
+            sender.CoreWebView2.Settings.IsStatusBarEnabled = false;
+            sender.CoreWebView2.Settings.IsSwipeNavigationEnabled = false;
+            sender.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+            sender.CoreWebView2.Settings.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; Cortana 1.14.4.19041; 10.0.0.0.19044.1645) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19045";
+            sender.NavigationStarting += WebView2Control_NavigationStarting;
+            sender.NavigationCompleted += WebView2Control_NavigationCompleted;
+            sender.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+        }
+
+        private void WebView2Control_NavigationStarting(muxc.WebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationStartingEventArgs args)
+        {
+            ProgressRing.Visibility = Visibility.Visible;
+        }
+
+        private void WebView2Control_NavigationCompleted(muxc.WebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs args)
+        {
+            ProgressRing.Visibility = Visibility.Collapsed;
+        }
+
+        private async void CoreWebView2_NewWindowRequested(Microsoft.Web.WebView2.Core.CoreWebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NewWindowRequestedEventArgs args)
+        {
+            string url = args.Uri;
+            await Launcher.LaunchUriAsync(new Uri(url));
+            args.Handled = true;
+        }
+    }
+}
